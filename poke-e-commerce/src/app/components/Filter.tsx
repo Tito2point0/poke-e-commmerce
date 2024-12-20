187787e38c027009f99c148bfbb9af9bbea3f7a2
Tito2point0 <defconx00@gmail.com>
2024-12-20 18:35:30 -0500

    added types value grading and sets finished display

--- poke-e-commerce/src/app/components/Filter.tsx
+++ poke-e-commerce/src/app/components/Filter.tsx
@@ -8,16 +8,25 @@ const Filter = () => {
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
         >
           <option>Type</option>
-          <option value="physical">Physical</option>
-          <option value="digital">Digital</option>
+          <option value="packs">Packs</option>
+          <option value="ETB">Elite Trainer Boxes</option>
+          <option value="booster">Booster Boxes</option>
+          <option value="tins">Tins</option>
+          <option value="singles">Singles</option>
+          <input type="text" name="min" placeholder="min price" className="text-xs rounded-2xl pl-2 w-24 ring-1 ring-grey-400"/>
         </select>
         <select
-          name="size"
+          name="Value"
           id=""
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
         >
-          <option>Set</option>
-          <option value="set">Set</option>
+          <option>Value</option>
+          <option value="-5000">Under 5000</option>
+          <option value="-1000">Under 1000</option>
+          <option value="-500">Under 500</option>
+          <option value="-100">Under 100</option>
+          <option value="-50">Under 50</option>
+
           
         </select>
         <select
@@ -26,16 +35,25 @@ const Filter = () => {
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
         >
           <option>Grade</option>
-          <option value="grade">Physical</option>
+          <option value="10">10</option>
+          <option value="9">9</option>
+          <option value="8">8</option>
+          <option value="7">7</option>
+          <option value="6">6</option>
+
         </select>
         <select
-          name="type"
+          name="set"
           id=""
           className="py-2 px-4 rounded-2xl text-xs font-medium bg-[#EBEDED]"
         >
-          <option>Type</option>
-          <option value="physical">Physical</option>
-          <option value="digital">Digital</option>
+          <option>Set</option>
+          <option value="Paldean">Paldean Fates</option>
+          <option value="Temporal">Temporal Forces</option>
+          <option value="Twilight">Twilight Masquerade</option>
+          <option value="Shrouded">Shrouded Fable</option>
+          <option value="Stellar">Stellar Crown</option>
+          <option value="Surging">Surging Sparks</option>
         </select>
       </div>
       <div className=""></div>
