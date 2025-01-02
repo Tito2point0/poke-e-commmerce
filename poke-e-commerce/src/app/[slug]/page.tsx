be4e0dd1fbff03d4522629f6269ce80140b52d4c
Tito2point0 <defconx00@gmail.com>
2025-01-02 18:45:19 -0500

    subtracted un unnecessary padding added to state need to work on catagoryies ready to start back end

--- poke-e-commerce/src/app/[slug]/page.tsx
+++ poke-e-commerce/src/app/[slug]/page.tsx
@@ -4,7 +4,7 @@ import ProductImages from "../components/ProductImages";
 
 const SinglePage = () => {
   return (
-    <div className="min-h-screen flex flex-col pb-40"> {/* Ensures full viewport height and adds padding to bottom */}
+    <div className="min-h-screen flex flex-col "> {/* Ensures full viewport height and adds padding to bottom */}
       <div className="flex-grow px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
         {/* IMG */}
         <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
@@ -18,45 +18,42 @@ const SinglePage = () => {
             pellentesque cubilia habitant montes vitae dolor. Viverra nam eros
             erat purus penatibus. Diam eu rhoncus dapibus elit mauris.
           </p>
-          <div className="h-[2px] bg-gray-100">
-            <div className="flex items-center gap-4">
-              <h3 className="text-xl text-gray-500 line-through">$59</h3>
-              <h2 className="font-medium text-2xl">$49</h2>
-              <CustomizeProducts />
-              <AddList />
-            </div>
-            <div className="h-[2px] bg-gray-100"></div>
-            <div className="text-sm">
-              <h4 className="font-medium mb-4">Title</h4>
-              <p>
-                Lorem ipsum odor amet, consectetuer adipiscing elit. Tortor
-                pellentesque cubilia habitant montes vitae dolor. Viverra nam eros
-                erat purus penatibus. Diam eu rhoncus dapibus elit mauris.
-              </p>
-            </div>
-            <div className="h-[2px] bg-gray-100"></div>
-            <div className="text-sm">
-              <h4 className="font-medium mb-4">Title</h4>
-              <p>
-                Lorem ipsum odor amet, consectetuer adipiscing elit. Tortor
-                pellentesque cubilia habitant montes vitae dolor. Viverra nam eros
-                erat purus penatibus. Diam eu rhoncus dapibus elit mauris.
-              </p>
-            </div>
-            <div className="h-[2px] bg-gray-100"></div>
-            <div className="text-sm mb-32">
-              <h4 className="font-medium mb-4">Title</h4>
-              <p>
-                Lorem ipsum odor amet, consectetuer adipiscing elit. Tortor
-                pellentesque cubilia habitant montes vitae dolor. Viverra nam eros
-                erat purus penatibus. Diam eu rhoncus dapibus elit mauris.
-              </p>
-            </div>
+          <div className="h-[2px] bg-gray-100"></div>
+          <div className="flex flex-col gap-6"> {/* Stack CustomizeProducts and AddList vertically */}
+            <CustomizeProducts />
+            <AddList />
+          </div>
+          <div className="h-[2px] bg-gray-100"></div>
+          <div className="text-sm">
+            <h4 className="font-medium mb-4">Title</h4>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Tortor
+              pellentesque cubilia habitant montes vitae dolor. Viverra nam eros
+              erat purus penatibus. Diam eu rhoncus dapibus elit mauris.
+            </p>
+          </div>
+          <div className="h-[2px] bg-gray-100"></div>
+          <div className="text-sm">
+            <h4 className="font-medium mb-4">Title</h4>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Tortor
+              pellentesque cubilia habitant montes vitae dolor. Viverra nam eros
+              erat purus penatibus. Diam eu rhoncus dapibus elit mauris.
+            </p>
+          </div>
+          <div className="h-[2px] bg-gray-100"></div>
+          <div className="text-sm mb-32">
+            <h4 className="font-medium mb-4">Title</h4>
+            <p>
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Tortor
+              pellentesque cubilia habitant montes vitae dolor. Viverra nam eros
+              erat purus penatibus. Diam eu rhoncus dapibus elit mauris.
+            </p>
           </div>
         </div>
       </div>
       {/* Spacer to Separate Footer */}
-      <div className="h-40"></div> {/* Increased height to push footer down */}
+      <div className=""></div> {/* Increased height to push footer down */}
     </div>
   );
 };
